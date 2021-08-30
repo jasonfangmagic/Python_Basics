971e4de37300ad6eb97c634d3d50d84827f1e3ad
Jason Fang <jasonfangmagic@gmail.com>
2021-08-30 12:14:35 -0400

    Basics

--- basics.py
+++ basics.py
@@ -1,1 +1,70 @@
-print("Hello, World!")
+#Input
+print("Hello what's your name")
+name = input()
+print("Hello,", name)
+
+#operator
+num1 = 34
+num2 = 3
+
+#only have integer
+print(num1 // num2)
+
+#only residual
+print(num1 % num2)
+
+#expotion
+print(num1 ** num2)
+
+
+#convert numbers
+
+print("pick a number")
+
+num1 = input()
+
+print("pick another number")
+
+num2 = input()
+
+sum = int(num1) + int(num2)
+
+print("Your Number is", sum)
+
+
+print(2 == 4)
+
+age = input("Input your age; ")
+
+if int(age) == 15:
+    print("Hey, your age is 15")
+
+else: print("You're not 15")
+
+
+#Chained Conditional and Nested if statement
+x =3
+y = 9
+if not (y == x or x +y == 6):
+    print("run")
+
+else:
+    print(":(")
+
+
+if x == 3:
+    if y == 4:
+        print("x = 3, y = 4")
+    else:
+        print("x = 2, y != 4")
+else:
+    print("x != 3")
+
+
+#for loop
+
+for x in range(0, 10): #start, stop, step
+    print(x)
+
+
+
