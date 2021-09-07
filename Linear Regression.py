a65a761616ed5c05f943f0395fe71c753e693464
Jason Fang <jasonfangmagic@gmail.com>
2021-09-07 16:43:27 -0400

    Basics

--- Linear Regression.py
+++ Linear Regression.py
@@ -5,6 +5,9 @@ from sklearn import linear_model
 from sklearn.utils import shuffle
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
@@ -18,7 +21,36 @@ X = np.array(data.drop([predict], 1))
 
 Y = np.array(data[predict])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
 
+# linear = linear_model.LinearRegression()
+#
+# linear.fit(x_train, y_train)
+#
+# acc = linear.score(x_test, y_test)
 
+# print(acc)
+
+
+#save the model
+# with open("student model.pickle", "wb") as f:
+#     pickle.dump(linear, f)
+
+pickle_in = open("student model.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print("Co: \n", linear.coef_)
+print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
+
+linear.fit(x_train, y_train)
+
+acc = linear.score(x_test, y_test)
+
+print(acc)
