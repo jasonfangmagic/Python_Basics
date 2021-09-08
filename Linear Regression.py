203b502d3e59bba48541c38932143e7794ba52f9
Jason Fang <jasonfangmagic@gmail.com>
2021-09-08 09:38:01 -0400

    Linear Regression

--- Linear Regression.py
+++ Linear Regression.py
@@ -22,9 +22,9 @@ X = np.array(data.drop([predict], 1))
 Y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
-best = 0
 
 #save the best model
+# best = 0
 
 # for _ in range(30):
 #
@@ -70,4 +70,3 @@ pyplot.scatter(data[p], data["G3"])
 pyplot.xlabel(p)
 pyplot.ylabel("Final Grade")
 pyplot.show()
-
