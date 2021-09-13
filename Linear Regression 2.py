e7b9bcd2f05654b0a6bffebb94d1ee92d65536d9
Jason Fang <jasonfangmagic@gmail.com>
2021-09-13 17:41:16 -0400

    Linear Regression

--- Linear Regression 2.py
+++ Linear Regression 2.py
@@ -96,3 +96,41 @@ plt.xlabel ('Date')
 plt.ylabel ('Price')
 plt.show()
 
+
+#algorithm
+
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+ys = np.array([5,4,2,3,5,7], dtype=np.float64)
+plt.scatter(xs,ys)
+plt.show()
+
+def best_fit_slope(xs, ys):
+    m =( ((mean(xs)*mean(ys)) - mean(xs*ys)) /
+          ((mean(xs)*mean(xs)) - mean(xs*xs)))
+
+    b = mean(ys) - m*mean(xs)
+    return m, b
+
+m,b = best_fit_slope(xs, ys)
+
+
+
+print(m, b)
+
+regression_line = [(m*x)+b for x in xs]
+
+
+#the same
+for x in xs:
+    regression_line.append((m*x)+b)
+
+plt.scatter(xs, ys)
+plt.plot(xs, regression_line)
+plt.show()
