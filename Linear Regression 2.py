b326909be8bd3793c4c86dbf0d05a95abc075577
Jason Fang <jasonfangmagic@gmail.com>
2021-09-14 11:35:32 -0400

    Linear Regression

--- Linear Regression 2.py
+++ Linear Regression 2.py
@@ -128,10 +128,22 @@ regression_line = [(m*x)+b for x in xs]
 
 
 #the same
-for x in xs:
-    regression_line.append((m*x)+b)
+# for x in xs:
+#     regression_line.append((m*x)+b)
 
 #same
 plt.scatter(xs, ys)
 plt.plot(xs, regression_line)
 plt.show()
+
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig)**2)
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+r_squared = coefficient_of_determination(ys, regression_line)
+print(r_squared)
