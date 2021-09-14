65ebd0f272e795efd04665479dfff196179126f0
Jason Fang <jasonfangmagic@gmail.com>
2021-09-14 13:17:55 -0400

    Linear Regression

--- Linear Regression Algorithm Building.py
+++ Linear Regression Algorithm Building.py
@@ -1,4 +1,80 @@
+
+#Linear Regression Algorithm Building
+
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+import random
+
+style.use('fivethirtyeight')
+
+# xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+# ys = np.array([5,4,2,3,5,7], dtype=np.float64)
+
+def create_dataset (hm, variance, step = 2, correlation = False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val+=step
+        elif correlation and correlation == 'neg':
+            val -= step
+    xs = [i for i in range(len(ys))]
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
+
+xs, ys = create_dataset(100, 80, 2, correlation= 'pos')
+
+plt.scatter(xs,ys)
+plt.show()
+
+def best_fit_slope(xs, ys):
+    m =( ((mean(xs)*mean(ys)) - mean(xs*ys)) /
+          ((mean(xs)*mean(xs)) - mean(xs*xs)))
+
+    b = mean(ys) - m*mean(xs)
+    return m, b
+
+m,b = best_fit_slope(xs, ys)
+
+
+
+print(m, b)
+
+regression_line = [(m*x)+b for x in xs]
+
+
+#the same
+# for x in xs:
+#     regression_line.append((m*x)+b)
+
+#same
+plt.scatter(xs, ys)
+plt.plot(xs, regression_line)
+plt.show()
+
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig)**2)
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+r_squared = coefficient_of_determination(ys, regression_line)
+print(r_squared)
+
+
+
+
+
+
+
 import math
+import random
 
 import pandas as pd
 import quandl
@@ -95,58 +171,4 @@ plt.legend(loc = 4)
 plt.xlabel ('Date')
 plt.ylabel ('Price')
 plt.show()
-
-
-#algorithm
-
-from statistics import mean
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
-
-style.use('fivethirtyeight')
-
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,2,3,5,7], dtype=np.float64)
-plt.scatter(xs,ys)
-plt.show()
-
-def best_fit_slope(xs, ys):
-    m =( ((mean(xs)*mean(ys)) - mean(xs*ys)) /
-          ((mean(xs)*mean(xs)) - mean(xs*xs)))
-
-    b = mean(ys) - m*mean(xs)
-    return m, b
-
-m,b = best_fit_slope(xs, ys)
-
-
-
-print(m, b)
-
-regression_line = [(m*x)+b for x in xs]
-
-
-#the same
-# for x in xs:
-#     regression_line.append((m*x)+b)
-
-#same
-plt.scatter(xs, ys)
-plt.plot(xs, regression_line)
-plt.show()
-
-def squared_error(ys_orig, ys_line):
-    return sum((ys_line - ys_orig)**2)
-
-def coefficient_of_determination(ys_orig, ys_line):
-    y_mean_line = [mean(ys_orig) for y in ys_orig]
-    squared_error_regr = squared_error(ys_orig, ys_line)
-    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
-    return 1 - (squared_error_regr / squared_error_y_mean)
-
-r_squared = coefficient_of_determination(ys, regression_line)
-print(r_squared)
-
-
 
