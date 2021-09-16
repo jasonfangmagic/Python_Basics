7e638dade731d0b4dd8d8dee29d002c3ad64557d
Jason Fang <jasonfangmagic@gmail.com>
2021-09-16 16:08:19 -0400

    svm

--- SVM Algorithm Implementation.py
+++ SVM Algorithm Implementation.py
@@ -1,3 +1,50 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+style.use('ggplot')
+
+data_dict = {-1: np.array([[1, 7],
+                           [2, 8],
+                           [3, 8], ]),
+             1: np.array([[5, 1],
+                          [6, -1],
+                          [7, 3], ])}
+
+class Support_Vector_Machine:
+    def __init__(self, visualization=True):
+        self.visualization = visualization
+        self.colors = {1:'r',-1:'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1,1,1)
+
+        # train
+    def fit(self, data):
+        pass
+
+    def predict(self, features):
+        # sign( x.w+b )
+        classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+
+        return classification
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import KNN
 import numpy as np
 from sklearn import preprocessing, model_selection, svm, neighbors
@@ -20,3 +67,5 @@ clf.fit(X_train, Y_train)
 ACC = clf.score(X_test, Y_test)
 
 print(ACC)
+
+
