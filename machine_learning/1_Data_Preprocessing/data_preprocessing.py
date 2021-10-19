37b18202bf18029278292a05bd29bc76300e6388
jasonfangmagic <jasonfangmagic@gmail.com>
2021-10-19 17:02:35 -0400

    Python

--- machine_learning/1_Data_Preprocessing/data_preprocessing.py
+++ machine_learning/1_Data_Preprocessing/data_preprocessing.py
@@ -27,6 +27,15 @@ y = le.fit_transform(y)
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1) #same data
 
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+#don't standardize dummy variables
+x_train[:,3:] = sc.fit_transform(x_train[:,3:])
+x_test[:,3:] = sc.transform(x_test[:,3:])
+
+
+
 
 
 
