203eeedfab31781819ac26e68f2e3f459cd087ca
jasonfangmagic <jasonfangmagic@gmail.com>
2021-10-21 16:16:14 -0400

    Python

---
+++ machine_learning/2_Regression/SVR.py
@@ -0,0 +1,68 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('machine_learning/2_Regression/P14-Part2-Regression/Section 8 - Polynomial Regression/Python/Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+#feature scaling
+print(X)
+print(y)
+
+#change shape
+y = y.reshape(len(y),1)
+print(y)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+print(X)
+print(y)
+
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+print(y)
+
+y_pred = regressor.predict(X)
+print(y_pred)
+
+acc = regressor.score(X, y)
+print(acc)
+
+#r square
+from sklearn.metrics import r2_score
+
+coefficient_of_dermination = r2_score(y, y_pred)
+print(coefficient_of_dermination)
+
+
+#reverse transformaion
+sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])))
+
+# Visualising the SVR results
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X)), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
+plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid))), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
