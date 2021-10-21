767a54e4f8a5961cbc407fb7d5ca353379c6926a
jasonfangmagic <jasonfangmagic@gmail.com>
2021-10-20 22:50:58 -0400

    Python

--- machine_learning/2_Regression/P14-Part2-Regression/Section 7 - Multiple Linear Regression/Python/multiple_linear_regression.py
+++ machine_learning/2_Regression/P14-Part2-Regression/Section 7 - Multiple Linear Regression/Python/multiple_linear_regression.py
@@ -43,10 +43,10 @@ print(regressor.coef_)
 print(regressor.intercept_)
 
 # Building the optimal model using Backward Elimination
-import statsmodels.formula.api as sm
-X=np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
-X_opt=X[:, [0, 1, 2, 3, 4, 5]]
-regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary() X_opt=X[:, [0, 1, 3, 4, 5]] regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary() X_opt=X[:, [0, 3, 4, 5]]
-regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary() X_opt=X[:, [0, 3, 5]]
-regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary() X_opt=X[:, [0, 3]]
-regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary()
+# import statsmodels.formula.api as sm
+# X=np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+# X_opt=X[:, [0, 1, 2, 3, 4, 5]]
+# regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary() X_opt=X[:, [0, 1, 3, 4, 5]] regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary() X_opt=X[:, [0, 3, 4, 5]]
+# regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary() X_opt=X[:, [0, 3, 5]]
+# regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary() X_opt=X[:, [0, 3]]
+# regressor_OLS=sm.OLS(endog=y, exog=X_opt).fit() regressor_OLS.summary()
