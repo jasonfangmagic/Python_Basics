fd82543ff8a7372d024abb112e751369fcfc29da
jasonfangmagic <jasonfangmagic@gmail.com>
2021-10-25 22:26:29 -0400

    Python

--- machine_learning/8_Deep_Learning/ANN.py
+++ machine_learning/8_Deep_Learning/ANN.py
@@ -9,18 +9,18 @@ tf.__version__
 # Part 1 - Data Preprocessing
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('machine_learning/8_Deep_Learning/Section 35 - Artificial Neural Networks (ANN)/Python/Churn_Modelling.csv')
 X = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:, -1].values
 print(X)
 print(y)
 
-# Encoding categorical data
 # Label Encoding the "Gender" column
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 X[:, 2] = le.fit_transform(X[:, 2])
 print(X)
+
 # One Hot Encoding the "Geography" column
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
@@ -28,7 +28,8 @@ ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remaind
 X = np.array(ct.fit_transform(X))
 print(X)
 
-# Feature Scaling
+#X= X[:,1:]
+
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X = sc.fit_transform(X)
@@ -38,12 +39,13 @@ print(X)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Part 2 - Building the ANN
+X.std().round(2)
+X.mean().round(2)
 
-# Initializing the ANN
 ann = tf.keras.models.Sequential()
 
-# Adding the input layer and the first hidden layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
 ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 
 # Adding the second hidden layer
@@ -52,17 +54,12 @@ ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
 # Adding the output layer
 ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
-# Part 3 - Training the ANN
-
 # Compiling the ANN
 ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Training the ANN on the Training set
 ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
 
-# Part 4 - Making the predictions and evaluating the model
-
-# Predicting the Test set results
 y_pred = ann.predict(X_test)
 y_pred = (y_pred > 0.5)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
@@ -72,6 +69,16 @@ from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 
+#bayes
+# Training the SVM model on the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+print(y_pred)
+
 from sklearn.metrics import f1_score
 f1 = f1_score(y_test, y_pred, average='macro')
 print(f1)
@@ -80,3 +87,5 @@ from sklearn.metrics import accuracy_score
 acc=accuracy_score(y_test, y_pred)
 print(acc)
 
+print(ann.predict(sc.transform([[1,0,0,600,1,40,3,60000,2,1,1,50000]])))
+print(ann.predict(sc.transform([[1,0,0,600,1,40,3,60000,2,1,1,50000]]))>0.5)
