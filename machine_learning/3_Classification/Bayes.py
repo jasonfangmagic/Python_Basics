d8742501d15b7ee8eaf29d2df222ebcb1548433f
jasonfangmagic <jasonfangmagic@gmail.com>
2021-10-27 17:51:20 -0400

    Python

--- machine_learning/3_Classification/Bayes.py
+++ machine_learning/3_Classification/Bayes.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+print("hello world")
 # Importing the dataset
 dataset = pd.read_csv('machine_learning/3_Classification/Section 17 - K-Nearest Neighbors (K-NN)/Python/Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
